ca00c0ba6bfadd4da2dcb2e5adcaf1ea473a5b59
Martín Morandini <84515114+MMorandiniGit@users.noreply.github.com>
2022-05-01 23:21:43 -0300

    Add files via upload

---
+++ script-tp3.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+chmod ugo+x script-tp3.sh
+crontab -l > statusweb
+echo "0 * * * * ./script-tp3.sh"
+crontab statusweb
+rm statusweb
+while IFS= read -r site 
+do
+	if curl -I "$site" 2>&1 | grep -w "200\|301"; then
+	    echo "$site está activo"
+	else
+	    echo "$site está caído"
+	fi
+done < sitios > status_"$(date +%Y-%m-%d_%H_%M_%S)"
